c0befd776e265c99147fd422b24df6bb24680137
Jairo Ortega <jairo.161.ortega@gmail.com>
2023-01-25 09:59:42 -0600

    Agregado notfound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout";
-// import { NotFound } from "../pages/NotFound";
+import { NotFound } from "../pages/NotFound";
 import { Home } from "../pages/Home";
 import { AcercaDe } from "../pages/AcercaDe";
 import { Portafolio } from "../pages/Portafolio";
@@ -9,14 +9,14 @@ import { Contact } from "./Contact";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/Jairox789" element={<Home />} />
           <Route path="/acercade" element={<AcercaDe />} />
           <Route path="/portafolio" element={<Portafolio />} />
           <Route path="/contact" element={<Contact />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./styles/Home.css";
+
+function NotFound() {
+    return (
+        <>
+            <h1>No encontrado...</h1>
+        </>
+    );
+}
+
+export { NotFound };
