55715bbaf5115392128ec1a2f66bb48d89079583
Jairo <jairo.161.ortega@gmail.com>
2023-11-07 17:21:34 -0600

    Updates

---
+++ precache-manifest.ba721dbdc51dbde433bed5818ed88ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "935902422916f58263fa",
+    "url": "/static/css/main.89944f94.chunk.css"
+  },
+  {
+    "revision": "935902422916f58263fa",
+    "url": "/static/js/main.93590242.chunk.js"
+  },
+  {
+    "revision": "802b5bd497d2568dbeb3",
+    "url": "/static/js/1.802b5bd4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f1d35a83d4462c98293a168dee34e7b",
+    "url": "/static/media/logo.7f1d35a8.png"
+  },
+  {
+    "revision": "7f52ff500f553bdd92cbbd1233c91198",
+    "url": "/static/media/foto2.7f52ff50.jpg"
+  },
+  {
+    "revision": "d137c4c93c1e6a34c03a7358e85d7f18",
+    "url": "/index.html"
+  }
+];
